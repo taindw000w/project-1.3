7b232d31675da6142d37becfb717a4b21ee54fb3
taindw000w2 <taindw000w@gmail.com>
2022-04-28 17:46:33 +0700

    last commit

--- js/link-drop-down.js
+++ js/link-drop-down.js
@@ -2,7 +2,23 @@ const textBox = document.querySelector(".service-info__text");
 const textOpacity = document.querySelector(".opacity-text");
 const linkRead = document.querySelector(".continue-container__title");
 
-const activeLink = linkRead.addEventListener('click', function() {
+// containers
+const containerLogo = document.querySelector(".swiper-logo-container");
+const containerInfo = document.querySelector(".swiper-info-container");
+
+// container buttons
+const buttonActiveLogo = document.querySelector(".container-read-logo");
+const buttonActiveInfo = document.querySelector(".container-read-info");
+
+buttonActiveLogo.addEventListener("click", function () {
+  containerLogo.classList.toggle("swiper-logo-container_active");
+});
+buttonActiveInfo.addEventListener("click", function () {
+  containerInfo.classList.toggle("swiper-info-container_active");
+});
+
+const activeLink = linkRead.addEventListener("click", function () {
   textOpacity.classList.toggle("opacity-text__active");
   textBox.classList.toggle("service-info__text_active");
-})
+});
+
--- js/slider.js
+++ js/slider.js
@@ -2,11 +2,65 @@ const slider = document.querySelector(".swiper");
 const sliderSecond = document.querySelector(".swiper-second");
 const sliderThird = document.querySelector(".swiper-third");
 
+const swiper = new Swiper(slider, {
+  direction: "horizontal",
+  slidesPerView: 1.5,
+  loop: true,
+  spaceBetween: 25,
+  slidesPerColumn: 2,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    paginationClickable: true,
+  },
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 5,
+    },
+    768: {
+      autoHeight: false,
+      slidesPerView: 4,
+      spaceBetween: 5,
+      pagination: false,
+    },
+  },
+});
+const swiperSecond = new Swiper(sliderSecond, {
+  direction: "horizontal",
+  slidesPerView: 1.5,
+  loop: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination-second",
+    type: "bullets",
+    paginationClickable: true,
+  },
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 5,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 5,
+    },
+    860: {
+      slidesPerView: 4,
+      spaceBetween: 5,
+    },
+  },
+});
+
   const swiperThird = new Swiper(sliderThird, {
     direction: "horizontal",
-    slidesPerView: 1.5,
+    slidesPerView: 1.2,
     loop: true,
-    spaceBetween: 25,
+    spaceBetween: 20,
 
     // If we need pagination
     pagination: {
@@ -21,94 +75,90 @@ const sliderThird = document.querySelector(".swiper-third");
         slidesPerView: 2,
         spaceBetween: 5,
       },
-      620: {
-        slidesPerView: 2,
-        spaceBetween: 5,
-      },
     },
   });
 
 
-(function () {
-  const breakpoint = window.matchMedia("(min-width: 767px)");
-  const breakpointChecker = function () {
-    let swiper = undefined;
-    // if larger viewport and multi-row layout needed
-    if (breakpoint.matches === true) {
-      // clean up old instances and inline styles when available
-      if (swiper !== undefined) swiper.destroy(true, true);
-      // or/and do nothing
-      return;
-      // else if a small viewport and single column layout needed
-    } else if (breakpoint.matches === false) {
-      // fire small viewport version of swiper
-      return enableSwiper();
-    }
-  };
+// (function () {
+//   const breakpoint = window.matchMedia("(min-width: 767px)");
+//   const breakpointChecker = function () {
+//     let swiper = undefined;
+//     // if larger viewport and multi-row layout needed
+//     if (breakpoint.matches === true) {
+//       // clean up old instances and inline styles when available
+//       if (swiper !== undefined) swiper.destroy(true, true);
+//       // or/and do nothing
+//       return;
+//       // else if a small viewport and single column layout needed
+//     } else if (breakpoint.matches === false) {
+//       // fire small viewport version of swiper
+//       return enableSwiper();
+//     }
+//   };
 
-  const enableSwiper = function () {
-    swiper = new Swiper(slider, {
-      direction: "horizontal",
-      slidesPerView: 1.5,
-      loop: true,
-      spaceBetween: 25,
-      // If we need pagination
-      pagination: {
-        el: ".swiper-pagination",
-        type: "bullets",
-        paginationClickable: true,
-      },
-      breakpoints: {
-        480: {
-          slidesPerView: 2,
-          spaceBetween: 5,
-        },
-      },
-    });
-  };
-  breakpoint.addListener(breakpointChecker);
-  breakpointChecker();
-}());
+//   const enableSwiper = function () {
+//     swiper = new Swiper(slider, {
+//       direction: "horizontal",
+//       slidesPerView: 1.5,
+//       loop: true,
+//       spaceBetween: 25,
+//       // If we need pagination
+//       pagination: {
+//         el: ".swiper-pagination",
+//         type: "bullets",
+//         paginationClickable: true,
+//       },
+//       breakpoints: {
+//         480: {
+//           slidesPerView: 2,
+//           spaceBetween: 5,
+//         },
+//       },
+//     });
+//   };
+//   breakpoint.addListener(breakpointChecker);
+//   breakpointChecker();
+// }());
 
 
-(function () {
-  const breakpoint = window.matchMedia("(min-width: 768px)");
-  const breakpointChecker = function () {
-    let swiperSecond;
-    // if larger viewport and multi-row layout needed
-    if (breakpoint.matches === true) {
-      // clean up old instances and inline styles when available
-      if (swiperSecond !== undefined) swiperSecond.destroy(true, true);
-      // or/and do nothing
-      return;
-      // else if a small viewport and single column layout needed
-    } else if (breakpoint.matches === false) {
-      // fire small viewport version of swiper
-      return enableSwiper();
-    }
-  };
+// (function () {
+//   const breakpoint = window.matchMedia("(min-width: 768px)");
+//   const breakpointChecker = function () {
+//     let swiperSecond;
+//     // if larger viewport and multi-row layout needed
+//     if (breakpoint.matches === true) {
+//       // clean up old instances and inline styles when available
+//       if (swiperSecond !== undefined) swiperSecond.destroy(true, true);
+//       // or/and do nothing
+//       return;
+//       // else if a small viewport and single column layout needed
+//     } else if (breakpoint.matches === false) {
+//       // fire small viewport version of swiper
+//       return enableSwiper();
+//     }
+//   };
 
-  const enableSwiper = function () {
-    swiperSecond = new Swiper(sliderSecond, {
-      direction: "horizontal",
-      slidesPerView: 1.5,
-      loop: true,
-      spaceBetween: 25,
+//   const enableSwiper = function () {
+//     swiperSecond = new Swiper(sliderSecond, {
+//       direction: "horizontal",
+//       slidesPerView: 1.5,
+//       loop: true,
+//       spaceBetween: 25,
 
-      // If we need pagination
-      pagination: {
-        el: ".swiper-pagination-second",
-        type: "bullets",
-        paginationClickable: true,
-      },
-      breakpoints: {
-        480: {
-          slidesPerView: 2,
-          spaceBetween: 5,
-        },
-      },
-    });
-  };
-  breakpoint.addListener(breakpointChecker);
-  breakpointChecker();
-})();
+//       // If we need pagination
+//       pagination: {
+//         el: ".swiper-pagination-second",
+//         type: "bullets",
+//         paginationClickable: true,
+//       },
+//       breakpoints: {
+//         480: {
+//           slidesPerView: 2,
+//           spaceBetween: 5,
+//         },
+//       },
+//     });
+//   };
+//   breakpoint.addListener(breakpointChecker);
+//   breakpointChecker();
+// })();
